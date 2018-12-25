e267dd2815d12b47f6ca5fe370045e2259fba586
Daniel Moulson <dan24797@gmail.com>
2018-12-25 09:40:43 +0000

    initial

---
+++ ds3achievementtracker/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace ds3achievementtracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.programTitle = new System.Windows.Forms.Label();
+            this.ringMasterImage = new System.Windows.Forms.PictureBox();
+            this.allMasterImage = new System.Windows.Forms.PictureBox();
+            this.gestureMasterImage = new System.Windows.Forms.PictureBox();
+            this.infusionMasterImage = new System.Windows.Forms.PictureBox();
+            this.miracleMasterImage = new System.Windows.Forms.PictureBox();
+            this.pyroMasterImage = new System.Windows.Forms.PictureBox();
+            this.sorceryMasterImage = new System.Windows.Forms.PictureBox();
+            this.ringTotalStatus = new System.Windows.Forms.Panel();
+            this.sorceryTotalStatus = new System.Windows.Forms.Panel();
+            this.pyroTotalStatus = new System.Windows.Forms.Panel();
+            this.miracleTotalStatus = new System.Windows.Forms.Panel();
+            this.infusionTotalStatus = new System.Windows.Forms.Panel();
+            this.gestureTotalStatus = new System.Windows.Forms.Panel();
+            this.allTotalStatus = new System.Windows.Forms.Panel();
+            this.ringLabel = new System.Windows.Forms.Label();
+            this.ringProgressBar = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ringMasterImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.allMasterImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gestureMasterImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infusionMasterImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.miracleMasterImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pyroMasterImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sorceryMasterImage)).BeginInit();
+            this.ringTotalStatus.SuspendLayout();
+            this.sorceryTotalStatus.SuspendLayout();
+            this.pyroTotalStatus.SuspendLayout();
+            this.miracleTotalStatus.SuspendLayout();
+            this.infusionTotalStatus.SuspendLayout();
+            this.gestureTotalStatus.SuspendLayout();
+            this.allTotalStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ringProgressBar);
+            this.panel1.Controls.Add(this.ringLabel);
+            this.panel1.Location = new System.Drawing.Point(19, 145);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(671, 752);
+            this.panel1.TabIndex = 0;
+            // 
+            // programTitle
+            // 
+            this.programTitle.AutoSize = true;
+            this.programTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.programTitle.Location = new System.Drawing.Point(12, 9);
+            this.programTitle.Name = "programTitle";
+            this.programTitle.Size = new System.Drawing.Size(389, 37);
+            this.programTitle.TabIndex = 2;
+            this.programTitle.Text = "DS3 Achievement Tracker";
+            // 
+            // ringMasterImage
+            // 
+            this.ringMasterImage.Image = ((System.Drawing.Image)(resources.GetObject("ringMasterImage.Image")));
+            this.ringMasterImage.Location = new System.Drawing.Point(13, 12);
+            this.ringMasterImage.Name = "ringMasterImage";
+            this.ringMasterImage.Size = new System.Drawing.Size(65, 65);
+            this.ringMasterImage.TabIndex = 3;
+            this.ringMasterImage.TabStop = false;
+            // 
+            // allMasterImage
+            // 
+            this.allMasterImage.Image = ((System.Drawing.Image)(resources.GetObject("allMasterImage.Image")));
+            this.allMasterImage.Location = new System.Drawing.Point(13, 12);
+            this.allMasterImage.Name = "allMasterImage";
+            this.allMasterImage.Size = new System.Drawing.Size(65, 65);
+            this.allMasterImage.TabIndex = 4;
+            this.allMasterImage.TabStop = false;
+            // 
+            // gestureMasterImage
+            // 
+            this.gestureMasterImage.Image = ((System.Drawing.Image)(resources.GetObject("gestureMasterImage.Image")));
+            this.gestureMasterImage.Location = new System.Drawing.Point(12, 12);
+            this.gestureMasterImage.Name = "gestureMasterImage";
+            this.gestureMasterImage.Size = new System.Drawing.Size(65, 65);
+            this.gestureMasterImage.TabIndex = 5;
+            this.gestureMasterImage.TabStop = false;
+            // 
+            // infusionMasterImage
+            // 
+            this.infusionMasterImage.Image = ((System.Drawing.Image)(resources.GetObject("infusionMasterImage.Image")));
+            this.infusionMasterImage.Location = new System.Drawing.Point(12, 12);
+            this.infusionMasterImage.Name = "infusionMasterImage";
+            this.infusionMasterImage.Size = new System.Drawing.Size(65, 65);
+            this.infusionMasterImage.TabIndex = 6;
+            this.infusionMasterImage.TabStop = false;
+            // 
+            // miracleMasterImage
+            // 
+            this.miracleMasterImage.Image = ((System.Drawing.Image)(resources.GetObject("miracleMasterImage.Image")));
+            this.miracleMasterImage.Location = new System.Drawing.Point(13, 12);
+            this.miracleMasterImage.Name = "miracleMasterImage";
+            this.miracleMasterImage.Size = new System.Drawing.Size(65, 65);
+            this.miracleMasterImage.TabIndex = 7;
+            this.miracleMasterImage.TabStop = false;
+            // 
+            // pyroMasterImage
+            // 
+            this.pyroMasterImage.Image = ((System.Drawing.Image)(resources.GetObject("pyroMasterImage.Image")));
+            this.pyroMasterImage.Location = new System.Drawing.Point(13, 12);
+            this.pyroMasterImage.Name = "pyroMasterImage";
+            this.pyroMasterImage.Size = new System.Drawing.Size(65, 65);
+            this.pyroMasterImage.TabIndex = 8;
+            this.pyroMasterImage.TabStop = false;
+            // 
+            // sorceryMasterImage
+            // 
+            this.sorceryMasterImage.Image = ((System.Drawing.Image)(resources.GetObject("sorceryMasterImage.Image")));
+            this.sorceryMasterImage.Location = new System.Drawing.Point(13, 12);
+            this.sorceryMasterImage.Name = "sorceryMasterImage";
+            this.sorceryMasterImage.Size = new System.Drawing.Size(65, 65);
+            this.sorceryMasterImage.TabIndex = 9;
+            this.sorceryMasterImage.TabStop = false;
+            this.sorceryMasterImage.Click += new System.EventHandler(this.sorceryMasterImage_Click);
+            // 
+            // ringTotalStatus
+            // 
+            this.ringTotalStatus.BackColor = System.Drawing.Color.Red;
+            this.ringTotalStatus.Controls.Add(this.ringMasterImage);
+            this.ringTotalStatus.Location = new System.Drawing.Point(19, 49);
+            this.ringTotalStatus.Name = "ringTotalStatus";
+            this.ringTotalStatus.Size = new System.Drawing.Size(90, 90);
+            this.ringTotalStatus.TabIndex = 10;
+            // 
+            // sorceryTotalStatus
+            // 
+            this.sorceryTotalStatus.BackColor = System.Drawing.Color.Red;
+            this.sorceryTotalStatus.Controls.Add(this.sorceryMasterImage);
+            this.sorceryTotalStatus.Location = new System.Drawing.Point(115, 49);
+            this.sorceryTotalStatus.Name = "sorceryTotalStatus";
+            this.sorceryTotalStatus.Size = new System.Drawing.Size(90, 90);
+            this.sorceryTotalStatus.TabIndex = 11;
+            // 
+            // pyroTotalStatus
+            // 
+            this.pyroTotalStatus.BackColor = System.Drawing.Color.Red;
+            this.pyroTotalStatus.Controls.Add(this.pyroMasterImage);
+            this.pyroTotalStatus.Location = new System.Drawing.Point(211, 49);
+            this.pyroTotalStatus.Name = "pyroTotalStatus";
+            this.pyroTotalStatus.Size = new System.Drawing.Size(90, 90);
+            this.pyroTotalStatus.TabIndex = 12;
+            // 
+            // miracleTotalStatus
+            // 
+            this.miracleTotalStatus.BackColor = System.Drawing.Color.Red;
+            this.miracleTotalStatus.Controls.Add(this.miracleMasterImage);
+            this.miracleTotalStatus.Location = new System.Drawing.Point(307, 49);
+            this.miracleTotalStatus.Name = "miracleTotalStatus";
+            this.miracleTotalStatus.Size = new System.Drawing.Size(90, 90);
+            this.miracleTotalStatus.TabIndex = 12;
+            // 
+            // infusionTotalStatus
+            // 
+            this.infusionTotalStatus.BackColor = System.Drawing.Color.Red;
+            this.infusionTotalStatus.Controls.Add(this.infusionMasterImage);
+            this.infusionTotalStatus.Location = new System.Drawing.Point(403, 49);
+            this.infusionTotalStatus.Name = "infusionTotalStatus";
+            this.infusionTotalStatus.Size = new System.Drawing.Size(90, 90);
+            this.infusionTotalStatus.TabIndex = 12;
+            // 
+            // gestureTotalStatus
+            // 
+            this.gestureTotalStatus.BackColor = System.Drawing.Color.Red;
+            this.gestureTotalStatus.Controls.Add(this.gestureMasterImage);
+            this.gestureTotalStatus.Location = new System.Drawing.Point(499, 49);
+            this.gestureTotalStatus.Name = "gestureTotalStatus";
+            this.gestureTotalStatus.Size = new System.Drawing.Size(90, 90);
+            this.gestureTotalStatus.TabIndex = 12;
+            // 
+            // allTotalStatus
+            // 
+            this.allTotalStatus.BackColor = System.Drawing.Color.Red;
+            this.allTotalStatus.Controls.Add(this.allMasterImage);
+            this.allTotalStatus.Location = new System.Drawing.Point(600, 49);
+            this.allTotalStatus.Name = "allTotalStatus";
+            this.allTotalStatus.Size = new System.Drawing.Size(90, 90);
+            this.allTotalStatus.TabIndex = 12;
+            // 
+            // ringLabel
+            // 
+            this.ringLabel.AutoSize = true;
+            this.ringLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.ringLabel.Location = new System.Drawing.Point(3, 13);
+            this.ringLabel.Name = "ringLabel";
+            this.ringLabel.Size = new System.Drawing.Size(409, 39);
+            this.ringLabel.TabIndex = 0;
+            this.ringLabel.Text = "Master Of Rings Checklist";
+            // 
+            // ringProgressBar
+            // 
+            this.ringProgressBar.Location = new System.Drawing.Point(13, 56);
+            this.ringProgressBar.Name = "ringProgressBar";
+            this.ringProgressBar.Size = new System.Drawing.Size(646, 23);
+            this.ringProgressBar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1412, 1081);
+            this.Controls.Add(this.allTotalStatus);
+            this.Controls.Add(this.gestureTotalStatus);
+            this.Controls.Add(this.infusionTotalStatus);
+            this.Controls.Add(this.miracleTotalStatus);
+            this.Controls.Add(this.pyroTotalStatus);
+            this.Controls.Add(this.sorceryTotalStatus);
+            this.Controls.Add(this.programTitle);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ringTotalStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Dark Souls 3 Collection Tracker";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ringMasterImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.allMasterImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gestureMasterImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infusionMasterImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.miracleMasterImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pyroMasterImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sorceryMasterImage)).EndInit();
+            this.ringTotalStatus.ResumeLayout(false);
+            this.sorceryTotalStatus.ResumeLayout(false);
+            this.pyroTotalStatus.ResumeLayout(false);
+            this.miracleTotalStatus.ResumeLayout(false);
+            this.infusionTotalStatus.ResumeLayout(false);
+            this.gestureTotalStatus.ResumeLayout(false);
+            this.allTotalStatus.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label programTitle;
+        private System.Windows.Forms.PictureBox ringMasterImage;
+        private System.Windows.Forms.PictureBox allMasterImage;
+        private System.Windows.Forms.PictureBox gestureMasterImage;
+        private System.Windows.Forms.PictureBox infusionMasterImage;
+        private System.Windows.Forms.PictureBox miracleMasterImage;
+        private System.Windows.Forms.PictureBox pyroMasterImage;
+        private System.Windows.Forms.PictureBox sorceryMasterImage;
+        private System.Windows.Forms.Panel ringTotalStatus;
+        private System.Windows.Forms.Panel sorceryTotalStatus;
+        private System.Windows.Forms.Panel pyroTotalStatus;
+        private System.Windows.Forms.Panel miracleTotalStatus;
+        private System.Windows.Forms.Panel infusionTotalStatus;
+        private System.Windows.Forms.Panel gestureTotalStatus;
+        private System.Windows.Forms.Panel allTotalStatus;
+        private System.Windows.Forms.ProgressBar ringProgressBar;
+        private System.Windows.Forms.Label ringLabel;
+    }
+}
+
